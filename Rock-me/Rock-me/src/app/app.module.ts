ac78b5cd0cb95f8a5f03f52c3d9ec4c280162b32
TEGNY Thomas <thomas.tegny06@gmail.com>
2019-04-30 10:20:42 +0200

    Add my work on criterion2

--- Rock-me/Rock-me/src/app/app.module.ts
+++ Rock-me/Rock-me/src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import{FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { Criterion1Component } from './criterion1/criterion1.component';
+import { Criterion2Component } from './criterion2/criterion2.component';
+import { ProfileComponent } from './profile/profile.component';
+import { DiscussionComponent } from './discussion/discussion.component';
+import { SelectService } from './select.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Criterion1Component,
+    Criterion2Component,
+    ProfileComponent,
+    DiscussionComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [SelectService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
